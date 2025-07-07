c513f8a1c39b186b17340af63e5405c5b9da3ecd
TEJA <kuchiteja8@gmail.com>
2025-07-07 10:40:01 +0530

    java_fundamentals

---
+++ java_fundamentals/src/java_fundamentals/questio_3a.java
@@ -0,0 +1,17 @@
+package java_fundamentals;
+
+public class questio_3a {
+
+	public static void main(String[] args)
+	{
+		int num=5;
+		if (num>0)
+			System.out.println("The number is positive");
+		else if (num<0)
+			System.out.println("The number is negative" );
+		else
+			System.out.println("Zero");
+
+	}
+
+}
